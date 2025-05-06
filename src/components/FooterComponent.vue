ae534e3bc40227b6a1d83b328f58b91866e90427
Darya Filimonova <dasha220301@gmail.com>
2025-05-06 20:56:04 +0300

    Черновая верстка футера

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,52 @@
 <template>
   <v-footer
-      class="footer-component"
-      color="#D9F3D0"
+    class="footer-component"
+    color="#D9F3D0"
   >
-    <p>Ляляля это футер</p>
-    <p>Это ещё строка футера</p>
-    <p>И ещё..</p>
-    <p>..парочка</p>
+    <v-row class="justify-space-between" style="width: 90%; height: 100%">
+      <v-row>
+      <v-col>
+        <h6 >
+          <router-link
+            class="green-text text-left"
+            to="/contacts"
+            text="Контакты"
+          />
+        </h6>
+        <h6 >
+          <router-link
+            class="green-text"
+            to="/contacts"
+            text="Для организаторов"
+          />
+        </h6>
+      </v-col>
+      <v-col
+        class="text-left p-0"
+      >
+        <h6 class="green-text">
+          Наши соцсети:
+        </h6>
+        <a
+          target="_blank"
+          href="https://vk.com/polytech_orchestra"
+        >
+          <icon class="fa-brands fa-vk light-green-text icon-contacts"/>
+        </a>
+        <a
+          target="_blank"
+          href="https://t.me/piorchestra"
+        >
+          <icon class="fa-brands fa-telegram light-green-text icon-contacts"/>
+        </a>
+      </v-col>
+    </v-row>
+    <v-col>
+      <h6 class="green-text">Оркестр Политеха</h6>
+      <h6 class="green-text">© 2013 - 2024</h6>
+      <h6 class="green-text">Разработка сайта: Дарья Филимонова, Павел Жуков, Софья Подкорытова</h6>
+    </v-col>
+    </v-row>
   </v-footer>
 </template>
 
@@ -18,6 +58,11 @@ export default {
 
 <style scoped>
 .footer-component {
+  text-align: left;
+  background-color: #D9F3D0 !important;
+}
 
+.icon-contacts {
+  font-size: 20pt;
 }
 </style>
