cc3c60ba99ee527c59784580d84b9a827d3f80a6
Darya Filimonova <dasha220301@gmail.com>
2025-05-10 10:37:20 +0300

    Убрала неиспользуемые набор иконок

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,6 @@ import {aliases, mdi} from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import '@mdi/font/css/materialdesignicons.css'
-import '@fortawesome/fontawesome-free/css/all.css'
 
 const myCustomLightTheme = {
   dark: false,
